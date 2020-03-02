163019d00f52c3759c8faf4383a662e4f326d4c9
tanpn2402 <tanpn2402@gmail.com>
2020-03-02 13:28:29 +0700

    add authenticate and authorize

--- TestAPI/services/api.service.js
+++ TestAPI/services/api.service.js
@@ -1,5 +1,6 @@
 // index.js
 const ApiGateway = require("moleculer-web");
+const ERRORS = require("moleculer-web").Errors;
 const { ApolloService } = require("moleculer-apollo-server");
 let IO = require("socket.io");
 
@@ -24,13 +25,92 @@ module.exports = {
         routes: [
             {
                 path: '/api',
+                authentication: true,
+                authorization: true,
+                bodyParsers: {
+                    json: true,
+                    urlencoded: true
+                },
                 aliases: {
-                    // When the "GET /products" request is made the "listProducts" action of "products" service is executed
-                    "GET /products": "products.listProducts"
-                }
+                    "GET /products": "products.listProducts",
+                    "POST /products": "products.listProducts"
+                },
+                onBeforeCall(ctx, route, req, res) {
+                    this.logger.info("onBeforeCall in protected route");
+                    this.logger.info("user meta = ", ctx.meta.user);
+                    ctx.meta.authToken = req.headers["authorization"];
+                },
+
+                onAfterCall(ctx, route, req, res, data) {
+                    this.logger.info("onAfterCall in protected route");
+                    res.setHeader("X-Custom-Header", "Authorized path");
+
+                    return data;
+                },
             }
         ]
     },
+
+    methods: {
+        /**
+         * Please note that authenticate is called before authorize
+         * @param {*} ctx 
+         * @param {*} route 
+         * @param {*} req 
+         * @param {*} res 
+         */
+        authenticate(ctx, route, req, res) {
+            let accessToken = req.body["access_token"];
+            console.log('authenticate access_token = ', accessToken);
+
+            if (accessToken) {
+                if (accessToken === "123456") {
+                    // valid credentials
+                    return Promise.resolve({ id: 1, username: "john.doe", name: "John Doe", data: '123' });
+                } else {
+                    // invalid credentials
+                    return Promise.reject(new ERRORS.UnAuthorizedError(ERRORS.ERR_NO_TOKEN));
+                }
+            } else {
+                // anonymous user
+                return Promise.resolve(null);
+            }
+        },
+
+
+		/**
+		 * Authorize the request
+		 *
+		 * @param {Context} ctx
+		 * @param {Object} route
+		 * @param {IncomingRequest} req
+		 * @returns {Promise}
+		 */
+        authorize(ctx, route, req) {
+            console.log(ctx);
+            let auth = req.headers["authorization"];
+            if (auth && auth.startsWith("Bearer")) {
+                let token = auth.slice(7);
+
+                console.log('authorize token = ', token)
+                // Check the token
+                if (token == "123456") {
+                    // Set the authorized user entity to `ctx.meta`
+                    // ctx.meta.user = { id: 1, name: "John Doe" };
+                    return Promise.resolve(ctx);
+
+                } else {
+                    // Invalid token
+                    return Promise.reject(new ERRORS.UnAuthorizedError(ERRORS.ERR_INVALID_TOKEN));
+                }
+
+            } else {
+                // No token
+                return Promise.reject(new ERRORS.UnAuthorizedError(ERRORS.ERR_NO_TOKEN));
+            }
+        },
+    },
+
     started() {
         // Create a Socket.IO instance, passing it our server
         this.io = IO.listen(this.server);
--- TestAPI/services/product.service.js
+++ TestAPI/services/product.service.js
@@ -6,6 +6,7 @@ module.exports = {
     actions: {
         // Define service action that returns the available products
         listProducts(ctx) {
+            console.log('in products service, meta = ', ctx.meta)
             return [
                 { name: "Apples", price: 5 },
                 { name: "Oranges", price: 3 },
