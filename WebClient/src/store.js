a81bc35276ef2995ce4f9c05cf5aeb9426b92760
tanpn2402 <tanpn2402@gmail.com>
2020-03-13 13:59:19 +0700

    update

--- WebClient/src/store.js
+++ WebClient/src/store.js
@@ -4,8 +4,8 @@ import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { promiseMiddleware, localStorageMiddleware } from './middleware';
 import reducer from './reducer';
 
-import { routerMiddleware } from 'react-router-redux'
-import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createHashHistory';
 
 export const history = createHistory();
 
